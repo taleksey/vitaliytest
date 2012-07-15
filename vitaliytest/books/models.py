04737d6d3800818e76a64233f609683a973cf4c1
Aleksey Terehov <alterehov@gmail.com>
2012-07-15 14:50:10 +0300

    initial data and config admin part add tinyMCE

--- vitaliytest/books/models.py
+++ vitaliytest/books/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    FIO = models.CharField('max_length=255')
+    FIO = models.CharField(max_length=255)
     birthday = models.DateField('data birthday')
-    dieday = models.DateField('data died',blank=True)
+    dieday = models.DateField('data died',null=True, blank=True)
 
     def __unicode__(self):
         return self.FIO
 
 class Book(models.Model):
     author = models.ManyToManyField(Author)
-    title = models.CharField('max_length=100')
+    title = models.CharField(max_length=100)
     yearbook = models.DateField('year publish')
-    short_describe = models.CharField('max_length=255')
+    short_describe = models.TextField(max_length=500)
 
     def __unicode__(self):
         return self.title
--- vitaliytest/vitaliytest/settings.py
+++ vitaliytest/vitaliytest/settings.py
@@ -51,18 +51,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -122,9 +122,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+     'django.contrib.admindocs',
+    'books',
+    'tinymce',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -155,3 +157,11 @@ LOGGING = {
         },
     }
 }
+
+TINYMCE_DEFAULT_CONFIG = {
+    'plugins': "table,spellchecker,paste,searchreplace",
+    'theme': "advanced",
+
+    'theme_advanced_toolbar_location' : 'top',
+    }
+TINYMCE_SPELLCHECKER = True
