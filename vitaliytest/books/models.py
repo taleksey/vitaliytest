d7fc1375d11a7bd77a2522894c959a669bbc1cfc
Aleksey Terehov <alterehov@gmail.com>
2012-07-24 10:04:33 +0300

    Add signals on delete/update/insert models Author and Book

--- vitaliytest/books/models.py
+++ vitaliytest/books/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.forms import ModelForm
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
+from django.utils.timezone import  now
 
 from tinymce.widgets import TinyMCE
 
@@ -41,3 +46,41 @@ class BookForm(ModelForm):
         widgets = {
             'short_describe': TinyMCE(attrs={'cols': 80, 'rows': 20}, mce_attrs= settings.TINYMCE_DEFAULT_CONFIG)
         }
+
+class DBHistory(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    add_date = models.DateTimeField(null=True,blank=True)
+    change_date = models.DateTimeField(null=True,blank=True)
+    delete_date = models.DateTimeField(null=True,blank=True)
+
+@receiver(post_save, sender=Author)
+def author_save(sender, **kwargs):
+    created = kwargs.get('created',None)
+    instance = kwargs.get('instance',None)
+    if created:
+        DBHistory.objects.create(content_object=instance,add_date = now())
+    else:
+        DBHistory.objects.create(content_object=instance,change_date = now())
+
+@receiver(pre_delete, sender=Author)
+def author_delete(sender, **kwargs):
+    instance = kwargs.get('instance',None)
+    DBHistory.objects.create(content_object=instance,delete_date = now())
+
+@receiver(post_save, sender=Book)
+def book_save(sender, **kwargs):
+    created = kwargs.get('created',None)
+    instance = kwargs.get('instance',None)
+    if created:
+        DBHistory.objects.create(content_object=instance,add_date = now())
+    else:
+        DBHistory.objects.create(content_object=instance,change_date = now())
+
+
+@receiver(pre_delete, sender=Book)
+def book_delete(sender, **kwargs):
+    instance = kwargs.get('instance',None)
+    DBHistory.objects.create(content_object=instance,delete_date = now())
+
